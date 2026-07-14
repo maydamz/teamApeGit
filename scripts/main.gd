7cb184e929dc3bacfbf34b8d9c1eeeb542e03390
isforile <realforile@gmail.com>
2026-07-14 20:57:49 +0800

    DayNight

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,7 +1,16 @@
 extends Node
-
+@onready var day_night_sys = $DayNightSystem
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#test call for day night system
+	day_night_sys.changeTime("Morning")
+	await get_tree().create_timer(10).timeout
+	day_night_sys.changeTime("Midday")
+	await get_tree().create_timer(10).timeout
+	day_night_sys.changeTime("Sunset")
+	await get_tree().create_timer(10).timeout
+	day_night_sys.changeTime("Night")
+	await get_tree().create_timer(10).timeout
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
