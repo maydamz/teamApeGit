564fc44af7e8397967b211022534d7ef2ed61745
dripdood1 <allenpeterhalos@gmail.com>
2026-07-14 20:06:19 +0800

    dialogue tweaks
    added speaker name text

--- scripts/UI/dialogue.gd
+++ scripts/UI/dialogue.gd
@@ -1,14 +1,18 @@
 extends Control
 
 @onready var _dialogue : RichTextLabel = $dialogueTextContainer/dialogueText
+@onready var _speaker : RichTextLabel = $speakerName
 
 func display_line(line : String, type = "mc"):
 	if type == "mc":
 		_dialogue.text = line
+		_speaker.text = "You"
 	if type == "friend":
 		_dialogue.text = "[color=Khaki]" + line + "[/color]"
+		_speaker.text = "[color=Khaki]Cora[/color]"
 	if type == "npc":
 		_dialogue.text = "[color=Gold]" + line + "[/color]"
+		_speaker.text = "[color=Gold]Stranger[/color]"
 	_dialogue._ready()
 	open()
 
