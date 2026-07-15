fb374693c821c3fbcad5348f65b11bdde9c80ac3
dripdood1 <allenpeterhalos@gmail.com>
2026-07-15 20:31:34 +0800

    dialogue tweak

--- scripts/UI/dialogue.gd
+++ scripts/UI/dialogue.gd
@@ -5,6 +5,12 @@ extends Control
 @onready var _indicator: Label = $indicator
 @onready var _animator: AnimationPlayer = $indicator/transparencyController
 
+signal interactPress
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("interact"):
+		interactPress.emit()
+		
 func display_line(line : String, type = "mc"):
 	_animator.stop()
 	_indicator.modulate.a = 0
@@ -23,14 +29,6 @@ func display_line(line : String, type = "mc"):
 	_animator.play("reveal")
 	await _animator.animation_finished
 	_animator.play("blinking")
-	
-func dialogue_sc1() -> void:
-	display_line("ANDRO!", "friend")
-	display_line("Hey, what’s up? It’s my birthday today and you didn’t even greet me..", "friend")
-	display_line("Ulol")
-	display_line("Haaa… You got me… But really, you didn’t even say good morning earlier.", "friend")
-	display_line("You just passed by and went straight to your shop.", "friend")
-	pass
 
 func open():
 	visible = true
