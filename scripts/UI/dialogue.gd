227b481e91c4a6966cdc67c868e8e3082e7ed24b
dripdood1 <allenpeterhalos@gmail.com>
2026-07-15 18:21:03 +0800

    dialogue tweaking (NOT DONE)

--- scripts/UI/dialogue.gd
+++ scripts/UI/dialogue.gd
@@ -2,8 +2,11 @@ extends Control
 
 @onready var _dialogue : RichTextLabel = $dialogueTextContainer/dialogueText
 @onready var _speaker : RichTextLabel = $speakerName
+@onready var _indicator: Label = $indicator
+@onready var _animator: AnimationPlayer = $indicator/transparencyController
 
 func display_line(line : String, type = "mc"):
+	_indicator.visible = false
 	if type == "mc":
 		_dialogue.text = line
 		_speaker.text = "You"
@@ -15,6 +18,17 @@ func display_line(line : String, type = "mc"):
 		_speaker.text = "[color=Gold]Stranger[/color]"
 	_dialogue._ready()
 	open()
+	_indicator.visible = true
+	_animator.play("reveal")
+	_animator.play("blinking")
+	
+func dialogue_sc1() -> void:
+	display_line("ANDRO!", "friend")
+	display_line("Hey, what’s up? It’s my birthday today and you didn’t even greet me..", "friend")
+	display_line("Ulol")
+	display_line("Haaa… You got me… But really, you didn’t even say good morning earlier.", "friend")
+	display_line("You just passed by and went straight to your shop.", "friend")
+	pass
 
 func open():
 	visible = true
