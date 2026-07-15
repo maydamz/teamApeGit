94ec1ff10a98fb81313ef43f3b6b5d81cc23b3b6
dripdood1 <allenpeterhalos@gmail.com>
2026-07-15 15:49:59 +0800

    a

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,8 +1,10 @@
 extends Node
 @onready var day_night_sys = $DayNightSystem
+@onready var npc_scene = preload("res://scenes/spawned_npc.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#test call for day night system
+	spawnNpc()
 	day_night_sys.changeTime("Morning")
 	await get_tree().create_timer(10).timeout
 	day_night_sys.changeTime("Midday")
@@ -12,7 +14,14 @@ func _ready() -> void:
 	day_night_sys.changeTime("Night")
 	await get_tree().create_timer(10).timeout
 	pass
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func spawnNpc():
+	var follow_node = PathFollow3D.new()
+	$npcPath.add_child(follow_node)
+	
+	var new_npc = npc_scene.instantiate()
+	
+	follow_node.add_child(new_npc)
--- scripts/world/npc_movement.gd
+++ scripts/world/npc_movement.gd
@@ -1,1 +1,10 @@
 extends CharacterBody3D
+
+@export var speed: float = 150.0
+
+func _npc_walk(delta):
+	var path_follow = get_parent() as PathFollow3D
+	
+	if path_follow:
+		path_follow.progress += speed * delta
+		global_position = path_follow.global_position
