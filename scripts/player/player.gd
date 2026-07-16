818f5c0da71f79af39d2538c2053d84e505cf67c
matziiro <gavin.gallevo@gmail.com>
2026-07-16 21:13:20 +0800

    Add floor snap and improve movement code structure

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -32,6 +32,8 @@ var t_bob = 0.0
 var base_fov = 75.0
 const FOV_CHANGE = 1.0
 
+# Step / slope assistance
+const FLOOR_SNAP = 0.35
 
 # Accessing head and camera as variables
 @onready var head: Node3D = $Head
@@ -42,45 +44,67 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	add_to_group("player")
 
+	# Step / curb assistance
+	floor_snap_length = FLOOR_SNAP
+	safe_margin = 0.05
+	floor_max_angle = deg_to_rad(55.0)
+
 # First person camera controls
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		head.rotate_y(-event.relative.x * SENSITIVITY)
 		camera.rotate_x(-event.relative.y * SENSITIVITY)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
-		
+
 func _physics_process(delta: float) -> void:
 	# Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	
+	else:
+		# Helps stay attached when walking down slopes/stairs
+		apply_floor_snap()
+
 	# Sprint
 	if sprint_enabled == true and Input.is_action_pressed("sprint"):
 		print("DEBUG: Sprinting...")
 		speed = SPRINT_SPEED
 	else:
 		speed = WALK_SPEED
-	
+
 	# Directional movement controls
-	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	var direction := (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	
+	var input_dir := Input.get_vector(
+		"move_left",
+		"move_right",
+		"move_forward",
+		"move_backward"
+	)
+
+	var direction := (
+		head.transform.basis *
+		Vector3(input_dir.x, 0, input_dir.y)
+	).normalized()
+
 	if direction:
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
 	else:
 		velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
 		velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
-	
+
 	# Enable head bobbing
 	t_bob += delta * velocity.length() * float(is_on_floor())
 	camera.transform.origin = _headbob(t_bob)
-		
+
 	# FOV effects
-	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
+	var velocity_clamped = clamp(
+		velocity.length(),
+		0.5,
+		SPRINT_SPEED * 2
+	)
+
 	var target_fov = base_fov + FOV_CHANGE * velocity_clamped
 	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
-	
+
 	# Collision handling with other bodies
 	move_and_slide()
 
